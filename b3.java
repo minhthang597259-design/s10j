3fb8b825460684f34e1cb57e0352008b7a18c7cc
minhthang597259-design <minhthang597259@gmail.com>
2026-05-14 10:52:45 +0700

    Update b3.java

--- b3.java
+++ b3.java
@@ -1,13 +1,32 @@
-package btvnss1;
-import java.util.Scanner;
-public class b3 {
+class Computer {
+
+    public double calculatePrice(double basePrice) {
+        System.out.println("[Using basePrice only]");
+        return basePrice;
+    }
+
+    public double calculatePrice(double basePrice, double tax) {
+        System.out.println("[Using basePrice + tax]");
+        return basePrice + tax;
+    }
+
+    public double calculatePrice(double basePrice, double tax, double discount) {
+        System.out.println("[Using basePrice + tax + discount]");
+        return basePrice + tax - discount; 
+    }
+}
+
+public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a,b,c,d;
-        a=sc.nextInt();
-        b=sc.nextInt();
-        c=sc.nextInt();
-        d=sc.nextInt();
-        System.out.println(((a*d)+(b*c)) +"/"+ (b*d));
+        Computer comp = new Computer();
+
+        double price1 = comp.calculatePrice(1000.0);
+        System.out.println("Final Price = " + price1 + "\n");
+
+        double price2 = comp.calculatePrice(1000.0, 100.0);
+        System.out.println("Final Price = " + price2 + "\n");
+
+        double price3 = comp.calculatePrice(1000.0, 100.0, 50.0);
+        System.out.println("Final Price = " + price3 + "\n");
     }
 }
