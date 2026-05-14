604cd40fe45d0878f77f6e2aafd90751ab6debbd
minhthang597259-design <minhthang597259@gmail.com>
2026-05-14 10:48:25 +0700

    Update b1.java

--- b1.java
+++ b1.java
@@ -1,10 +1,20 @@
-package btvnss1;
-import java.util.Scanner;
-public class b1 {
+class Animal {
+    protected String name;
+
+    public void makeSound() {
+        System.out.println("Some animal sound");
+    }
+}
+
+class Dog extends Animal {
+    public Dog(String name) {
+        this.name = name; 
+    }
+}
+
+public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("nhap ban kinh : ");
-        double r = sc.nextDouble();
-        System.out.printf("dien tich : %.2f\n", Math.PI * r * r);
+        Dog d = new Dog("Buddy");
+        d.makeSound();
     }
 }
